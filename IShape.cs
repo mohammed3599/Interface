32b5cb50682534d284f32485a5fd28ccfaec300a
mohammed3599 <kataloni1999@gmail.com>
2023-06-21 13:45:43 +0400

    Adding interface

--- IShape.cs
+++ IShape.cs
@@ -8,5 +8,34 @@ namespace Interface
 {
     internal interface IShape
     {
+        public double getArea();
+    }
+
+    class Rectangle : IShape
+    {
+        double hight;
+        double Width;
+        public Rectangle(double hight, double width)
+        {
+            this.hight = hight;
+            this.Width = width;
+        }
+        public double getArea()
+        {
+            return hight * Width;
+        }
+    }
+    class Circle : IShape
+    {
+
+        double Radius;
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+        public double getArea()
+        {
+            return (Math.PI * Radius * Radius);
+        }
     }
 }
